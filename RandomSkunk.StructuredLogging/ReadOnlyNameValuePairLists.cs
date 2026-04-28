2b67b8d89bd8fa2fe564de9b59dfd43e4bddda22
Brian Friesen <brian.friesen@nafinc.com>
2026-04-28 16:12:47 -0400

    LogPropertyTuple types are empty when default

--- RandomSkunk.StructuredLogging/ReadOnlyNameValuePairLists.cs
+++ RandomSkunk.StructuredLogging/ReadOnlyNameValuePairLists.cs
@@ -4,29 +4,6 @@ using System.Runtime.InteropServices;
 namespace RandomSkunk.StructuredLogging;
 
 /// <summary>
-/// Represents an empty, read-only array of name/value pairs.
-/// </summary>
-[StructLayout(LayoutKind.Auto)]
-public readonly struct EmptyNameValuePairArray : IReadOnlyList<KeyValuePair<string, object?>>
-{
-    /// <inheritdoc/>
-    public int Count => 0;
-
-    /// <inheritdoc/>
-    public KeyValuePair<string, object?> this[int index] => throw new IndexOutOfRangeException();
-
-    /// <summary>
-    /// Returns an enumerator that iterates through the collection of name-value pairs.
-    /// </summary>
-    /// <returns>An enumerator that can be used to iterate through the collection of name-value pairs.</returns>
-    public NameValuePairListEnumerator<EmptyNameValuePairArray> GetEnumerator() => new(this);
-
-    IEnumerator<KeyValuePair<string, object?>> IEnumerable<KeyValuePair<string, object?>>.GetEnumerator() => GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-}
-
-/// <summary>
 /// An immutable array-like structure of name-value pairs backed by an array of <see langword="object"/> log property tuples.
 /// </summary>
 [StructLayout(LayoutKind.Auto)]
@@ -43,7 +20,7 @@ public readonly struct LogPropertyArray : IReadOnlyList<KeyValuePair<string, obj
     public int Count => _logProperties.Length;
 
     /// <inheritdoc/>
-    public KeyValuePair<string, object?> this[int index] => new(_logProperties[index].Name, _logProperties[index].Value);
+    public KeyValuePair<string, object?> this[int index] => GetKeyValuePair(_logProperties[index]);
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -54,6 +31,9 @@ public readonly struct LogPropertyArray : IReadOnlyList<KeyValuePair<string, obj
     IEnumerator<KeyValuePair<string, object?>> IEnumerable<KeyValuePair<string, object?>>.GetEnumerator() => GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private static KeyValuePair<string, object?> GetKeyValuePair((string Name, object? Value) logProperty) =>
+        new(logProperty.Name, logProperty.Value);
 }
 
 /// <summary>
@@ -62,25 +42,29 @@ public readonly struct LogPropertyArray : IReadOnlyList<KeyValuePair<string, obj
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
     private readonly string _logPropertyKey;
     private readonly T _logPropertyValue;
 
     internal LogPropertyTuple(ref readonly (string Name, T Value) logProperty)
     {
+        _count = 1;
         _logPropertyKey = logProperty.Name;
         _logPropertyValue = logProperty.Value;
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 1;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logPropertyKey, _logPropertyValue),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logPropertyKey, _logPropertyValue),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -99,6 +83,8 @@ public readonly struct LogPropertyTuple<T> : IReadOnlyList<KeyValuePair<string, 
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T1, T2> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
+
     private readonly string _logProperty1Key;
     private readonly string _logProperty2Key;
 
@@ -109,6 +95,7 @@ public readonly struct LogPropertyTuple<T1, T2> : IReadOnlyList<KeyValuePair<str
         ref readonly (string Name, T1 Value) logProperty1,
         ref readonly (string Name, T2 Value) logProperty2)
     {
+        _count = 2;
         _logProperty1Key = logProperty1.Name;
         _logProperty2Key = logProperty2.Name;
         _logProperty1Value = logProperty1.Value;
@@ -116,16 +103,18 @@ public readonly struct LogPropertyTuple<T1, T2> : IReadOnlyList<KeyValuePair<str
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 2;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logProperty1Key, _logProperty1Value),
-            1 => new(_logProperty2Key, _logProperty2Value),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logProperty1Key, _logProperty1Value),
+                1 => new(_logProperty2Key, _logProperty2Value),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -144,6 +133,8 @@ public readonly struct LogPropertyTuple<T1, T2> : IReadOnlyList<KeyValuePair<str
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T1, T2, T3> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
+
     private readonly string _logProperty1Key;
     private readonly string _logProperty2Key;
     private readonly string _logProperty3Key;
@@ -157,6 +148,7 @@ public readonly struct LogPropertyTuple<T1, T2, T3> : IReadOnlyList<KeyValuePair
         ref readonly (string Name, T2 Value) logProperty2,
         ref readonly (string Name, T3 Value) logProperty3)
     {
+        _count = 3;
         _logProperty1Key = logProperty1.Name;
         _logProperty2Key = logProperty2.Name;
         _logProperty3Key = logProperty3.Name;
@@ -166,17 +158,19 @@ public readonly struct LogPropertyTuple<T1, T2, T3> : IReadOnlyList<KeyValuePair
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 3;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logProperty1Key, _logProperty1Value),
-            1 => new(_logProperty2Key, _logProperty2Value),
-            2 => new(_logProperty3Key, _logProperty3Value),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logProperty1Key, _logProperty1Value),
+                1 => new(_logProperty2Key, _logProperty2Value),
+                2 => new(_logProperty3Key, _logProperty3Value),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -195,6 +189,8 @@ public readonly struct LogPropertyTuple<T1, T2, T3> : IReadOnlyList<KeyValuePair
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T1, T2, T3, T4> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
+
     private readonly string _logProperty1Key;
     private readonly string _logProperty2Key;
     private readonly string _logProperty3Key;
@@ -211,6 +207,7 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4> : IReadOnlyList<KeyValue
         ref readonly (string Name, T3 Value) logProperty3,
         ref readonly (string Name, T4 Value) logProperty4)
     {
+        _count = 4;
         _logProperty1Key = logProperty1.Name;
         _logProperty2Key = logProperty2.Name;
         _logProperty3Key = logProperty3.Name;
@@ -222,18 +219,20 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4> : IReadOnlyList<KeyValue
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 4;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logProperty1Key, _logProperty1Value),
-            1 => new(_logProperty2Key, _logProperty2Value),
-            2 => new(_logProperty3Key, _logProperty3Value),
-            3 => new(_logProperty4Key, _logProperty4Value),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logProperty1Key, _logProperty1Value),
+                1 => new(_logProperty2Key, _logProperty2Value),
+                2 => new(_logProperty3Key, _logProperty3Value),
+                3 => new(_logProperty4Key, _logProperty4Value),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -252,6 +251,8 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4> : IReadOnlyList<KeyValue
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
+
     private readonly string _logProperty1Key;
     private readonly string _logProperty2Key;
     private readonly string _logProperty3Key;
@@ -271,6 +272,7 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5> : IReadOnlyList<KeyV
         ref readonly (string Name, T4 Value) logProperty4,
         ref readonly (string Name, T5 Value) logProperty5)
     {
+        _count = 5;
         _logProperty1Key = logProperty1.Name;
         _logProperty2Key = logProperty2.Name;
         _logProperty3Key = logProperty3.Name;
@@ -284,19 +286,21 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5> : IReadOnlyList<KeyV
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 5;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logProperty1Key, _logProperty1Value),
-            1 => new(_logProperty2Key, _logProperty2Value),
-            2 => new(_logProperty3Key, _logProperty3Value),
-            3 => new(_logProperty4Key, _logProperty4Value),
-            4 => new(_logProperty5Key, _logProperty5Value),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logProperty1Key, _logProperty1Value),
+                1 => new(_logProperty2Key, _logProperty2Value),
+                2 => new(_logProperty3Key, _logProperty3Value),
+                3 => new(_logProperty4Key, _logProperty4Value),
+                4 => new(_logProperty5Key, _logProperty5Value),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -315,6 +319,8 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5> : IReadOnlyList<KeyV
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
+
     private readonly string _logProperty1Key;
     private readonly string _logProperty2Key;
     private readonly string _logProperty3Key;
@@ -337,6 +343,7 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6> : IReadOnlyList<
         ref readonly (string Name, T5 Value) logProperty5,
         ref readonly (string Name, T6 Value) logProperty6)
     {
+        _count = 6;
         _logProperty1Key = logProperty1.Name;
         _logProperty2Key = logProperty2.Name;
         _logProperty3Key = logProperty3.Name;
@@ -352,20 +359,22 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6> : IReadOnlyList<
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 6;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logProperty1Key, _logProperty1Value),
-            1 => new(_logProperty2Key, _logProperty2Value),
-            2 => new(_logProperty3Key, _logProperty3Value),
-            3 => new(_logProperty4Key, _logProperty4Value),
-            4 => new(_logProperty5Key, _logProperty5Value),
-            5 => new(_logProperty6Key, _logProperty6Value),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logProperty1Key, _logProperty1Value),
+                1 => new(_logProperty2Key, _logProperty2Value),
+                2 => new(_logProperty3Key, _logProperty3Value),
+                3 => new(_logProperty4Key, _logProperty4Value),
+                4 => new(_logProperty5Key, _logProperty5Value),
+                5 => new(_logProperty6Key, _logProperty6Value),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -384,6 +393,8 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6> : IReadOnlyList<
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6, T7> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
+
     private readonly string _logProperty1Key;
     private readonly string _logProperty2Key;
     private readonly string _logProperty3Key;
@@ -409,6 +420,7 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6, T7> : IReadOnlyL
         ref readonly (string Name, T6 Value) logProperty6,
         ref readonly (string Name, T7 Value) logProperty7)
     {
+        _count = 7;
         _logProperty1Key = logProperty1.Name;
         _logProperty2Key = logProperty2.Name;
         _logProperty3Key = logProperty3.Name;
@@ -426,21 +438,23 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6, T7> : IReadOnlyL
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 7;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logProperty1Key, _logProperty1Value),
-            1 => new(_logProperty2Key, _logProperty2Value),
-            2 => new(_logProperty3Key, _logProperty3Value),
-            3 => new(_logProperty4Key, _logProperty4Value),
-            4 => new(_logProperty5Key, _logProperty5Value),
-            5 => new(_logProperty6Key, _logProperty6Value),
-            6 => new(_logProperty7Key, _logProperty7Value),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logProperty1Key, _logProperty1Value),
+                1 => new(_logProperty2Key, _logProperty2Value),
+                2 => new(_logProperty3Key, _logProperty3Value),
+                3 => new(_logProperty4Key, _logProperty4Value),
+                4 => new(_logProperty5Key, _logProperty5Value),
+                5 => new(_logProperty6Key, _logProperty6Value),
+                6 => new(_logProperty7Key, _logProperty7Value),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -459,6 +473,8 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6, T7> : IReadOnlyL
 [StructLayout(LayoutKind.Auto)]
 public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6, T7, T8> : IReadOnlyList<KeyValuePair<string, object?>>
 {
+    private readonly byte _count;
+
     private readonly string _logProperty1Key;
     private readonly string _logProperty2Key;
     private readonly string _logProperty3Key;
@@ -487,6 +503,7 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6, T7, T8> : IReadO
         ref readonly (string Name, T7 Value) logProperty7,
         ref readonly (string Name, T8 Value) logProperty8)
     {
+        _count = 8;
         _logProperty1Key = logProperty1.Name;
         _logProperty2Key = logProperty2.Name;
         _logProperty3Key = logProperty3.Name;
@@ -506,22 +523,24 @@ public readonly struct LogPropertyTuple<T1, T2, T3, T4, T5, T6, T7, T8> : IReadO
     }
 
     /// <inheritdoc/>
-    public readonly int Count => 8;
+    public readonly int Count => _count;
 
     /// <inheritdoc/>
     public readonly KeyValuePair<string, object?> this[int index] =>
-        index switch
-        {
-            0 => new(_logProperty1Key, _logProperty1Value),
-            1 => new(_logProperty2Key, _logProperty2Value),
-            2 => new(_logProperty3Key, _logProperty3Value),
-            3 => new(_logProperty4Key, _logProperty4Value),
-            4 => new(_logProperty5Key, _logProperty5Value),
-            5 => new(_logProperty6Key, _logProperty6Value),
-            6 => new(_logProperty7Key, _logProperty7Value),
-            7 => new(_logProperty8Key, _logProperty8Value),
-            _ => throw new IndexOutOfRangeException(),
-        };
+        _count == 0
+            ? throw new IndexOutOfRangeException()
+            : index switch
+            {
+                0 => new(_logProperty1Key, _logProperty1Value),
+                1 => new(_logProperty2Key, _logProperty2Value),
+                2 => new(_logProperty3Key, _logProperty3Value),
+                3 => new(_logProperty4Key, _logProperty4Value),
+                4 => new(_logProperty5Key, _logProperty5Value),
+                5 => new(_logProperty6Key, _logProperty6Value),
+                6 => new(_logProperty7Key, _logProperty7Value),
+                7 => new(_logProperty8Key, _logProperty8Value),
+                _ => throw new IndexOutOfRangeException(),
+            };
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
@@ -583,7 +602,10 @@ public readonly struct ReadOnlyNameValuePairList<TNameValuePairList> : IReadOnly
     public int Count => _logProperties?.Count ?? 0;
 
     /// <inheritdoc/>
-    public KeyValuePair<string, object?> this[int index] => _logProperties != null ? _logProperties[index] : throw new IndexOutOfRangeException();
+    public KeyValuePair<string, object?> this[int index] =>
+        _logProperties == null
+            ? throw new IndexOutOfRangeException()
+            : _logProperties[index];
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of name-value pairs.
